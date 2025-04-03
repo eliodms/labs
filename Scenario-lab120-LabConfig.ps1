3a26c3122a93f01d8bc61e1c2ab3bea59e9bb50c
eliodms <eliod@microsoft.com>
2025-04-03 16:15:26 +0200

    Create Scenario-lab120-LabConfig.ps1

---
+++ Scenario-lab120-LabConfig.ps1
@@ -0,0 +1,8 @@
+$LabConfig=@{ DomainAdminName='LabAdmin'; AdminPassword='LS1setup!' ; Prefix = '_' ; DCEdition='4' ; Internet=$true ; TelemetryLevel='None' ; TelemetryNickname='' ; AdditionalNetworksConfig=@(); VMs=@()} 
+
+# 2 HyperConverged Clusters 
+1..2 | ForEach-Object {$VMNames="1-S2D"; $LABConfig.VMs += @{ VMName = "$VMNames$_" ; Configuration = 'S2D' ; ParentVHD = 'Win2022_G2.vhdx'; SSDNumber = 0; SSDSize=800GB ; HDDNumber = 12; HDDSize= 4TB ; MemoryStartupBytes= 3GB ; StaticMemory=$true ; NestedVirt=$True}} 
+1..2 | ForEach-Object {$VMNames="2-S2D"; $LABConfig.VMs += @{ VMName = "$VMNames$_" ; Configuration = 'S2D' ; ParentVHD = 'Win2022_G2.vhdx'; SSDNumber = 0; SSDSize=800GB ; HDDNumber = 12; HDDSize= 4TB ; MemoryStartupBytes= 3GB ; StaticMemory=$true ; NestedVirt=$True}} 
+
+# 1 Standalone server 
+1 | ForEach-Object {$VMNames="Standalone"; $LABConfig.VMs += @{ VMName = "$VMNames$_" ; Configuration = 'S2D' ; ParentVHD = 'Win2022_G2.vhdx'; SSDNumber = 0; SSDSize=800GB ; HDDNumber = 12; HDDSize= 4TB ; MemoryStartupBytes= 2GB ; StaticMemory=$true ; NestedVirt=$True}} 
